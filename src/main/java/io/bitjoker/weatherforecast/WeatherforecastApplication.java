24a21f592b1edef6b275898885c2d5dd8bc6b6c8
Mauricio Valencia <mauriciovalencia@bitjoker.io>
2020-01-24 12:58:15 -0300

    First Commit

---
+++ src/main/java/io/bitjoker/weatherforecast/WeatherforecastApplication.java
@@ -0,0 +1,13 @@
+package io.bitjoker.weatherforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherforecastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherforecastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/bitjoker/weatherforecast/WeatherforecastApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bitjoker.weatherforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherforecastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
